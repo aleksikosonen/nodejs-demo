f3ae0c46272116d8dea585f34f81d2dceea06d95
alekskos <kososenaleksi@gmai.com>
2021-03-19 14:18:33 +0200

    added connection to database

--- models/catModel.js
+++ models/catModel.js
@@ -1,6 +1,29 @@
 // Model (usually gets data from database, in this case data is hard coded)
 'use strict';
 
+const pool = require('../database/db');
+const promisePool = pool.promise();
+
+const getAllCats = async () => {
+  try {
+    const [rows] = await promisePool.query('SELECT * FROM wop_cat');
+    console.log('somenthing back from db', rows);
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+  }
+};
+
+const getAllCatsSort = async (order) => {
+  try {
+    const [rows] = await promisePool.query('SELECT * FROM wop_cat ORDER BY ... ');
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+  }
+};
+
+/*
 const cats = [
   {
     id: '1',
@@ -19,7 +42,9 @@ const cats = [
     filename: 'http://placekitten.com/400/302',
   },
 ];
+*/
 
 module.exports = {
-  cats,
+  getAllCats,
+  getAllCatsSort,
 };
