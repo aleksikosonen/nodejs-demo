cc5082ed853df27df73efbf57fc925d5d8584221
alekskos <kososenaleksi@gmai.com>
2021-03-19 09:34:45 +0200

    Index js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Terve maailmankaikkeus</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
