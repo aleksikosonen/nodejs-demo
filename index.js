57945e748496e56c6e1d9e0c2c0cb274100bff09
alekskos <kososenaleksi@gmai.com>
2021-03-19 10:22:00 +0200

    added routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const catRouter = require('./routes/catRouter');
 
 app.use(express.static('public'));
 
@@ -10,25 +11,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Terve maailmankaikkeus</h1>');
 });
 
-app.get('/cat', (req, res) => {
-  console.log('get cat');
-  res.send('Hello Cat!');
-});
-
-app.post('/cat', (req, res) => {
-  console.log('post cat');
-  res.send('post cat');
-});
-
-app.put('/cat', (req, res) => {
-  console.log('put cat');
-  res.send('put cat');
-});
-
-app.delete('/cat', (req, res) => {
-  console.log('delete cat');
-  res.send('delete cat');
-});
+app.use('/cat', catRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
